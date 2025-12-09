dca1b51cea5baf53b8d4ec1e58c3a6486a0068a8
ElijahSun0104 <129723147+ElijahSun0104@users.noreply.github.com>
2025-12-09 12:13:34 +0800

    Implement HTML parsing with BeautifulSoup
    Added HTML parsing to extract prices and titles.

--- python.py
+++ python.py
@@ -9,3 +9,26 @@ if response.ok:
   print(response.text) #text属性会以string存储响应内容，因此可以直接获取网页源码
 else:
   print("请求失败")
+2.1解析html信息
+from bs4 import BeautifulSoup
+import requests
+content = requests.get("网页完整的url").text #将string存储在contenr中
+soup = BeautifulSoup(content,"html.parser") #将内容解析成树状结构
+all_prices = soup.findAll("p",attrs={"class":"price_color"}) #寻找共性属性
+for price in all_prices:
+  print(price.string)#可以在string后切片
+all_titles = soup.findAll("h3")
+for title in all_titles:
+  all_links = title.findAll("a")
+  for link in all_links:
+    print(link.string)
+2.2
+完整带for循环的代码：
+headers = {"User-Agent": "xxx"……}
+for start_num in range(0,250，25)：#结束值不包含在范围里
+ request.get("网页完整的url{start_num}",headers = headers)
+ content = requests.get("网页完整的url").text #将string存储在contenr中
+ soup = BeautifulSoup(content,"html.parser") #将内容解析成树状结构
+ all_prices = soup.findAll("p",attrs={"class":"price_color"}) #寻找共性属性
+ for price in all_prices:
+  print(price.string)#可以在string后切片
